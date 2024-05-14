6bc71f329bab0874afdcbf87f3735da562bd796b
reillymoonen <135081213+reillymoonen@users.noreply.github.com>
2024-05-15 09:59:35 +1200

    update

--- 00_MM_base_v01.py
+++ 00_MM_base_v01.py
@@ -37,9 +37,26 @@ def num_check(question):
 
         except ValueError:
             print("please enter am integer")
-
-
 # main routine starts here
+
+
+# Calculate the ticket price baseed on the age
+def calc_ticket_price(var_age):
+
+    # ticket is $7.50 for users under 16
+    if var_age < 16:
+        price = 7.5
+
+    # ticket is $10.50 for users between 16 and 64
+    elif var_age < 65:
+        price = 10.5
+
+    # ticket price is $6.50 for seniors (65+)
+    else:
+        price = 6.5
+
+    return price
+
 
 # set maximum number of tickets below
 MAX_TICKETS = 3
@@ -69,6 +86,10 @@ while tickets_sold < MAX_TICKETS:
     else:
         print("?? That looks like a type, please try again.")
         continue
+
+    # calculate ticket cost
+    ticket_cost = calc_ticket_price(age)
+    print("Age: {}, Ticket Price: ${:.2f}".format(age, ticket_cost))
 
     tickets_sold += 1
 
---
+++ 07_string_checker.py
@@ -0,0 +1,9 @@
+# checks that users enter a valid responce (eg yes / no
+# cash / credit) based on a list of options
+def string_checker(question, num_letters, valid_responses):
+    pass
+
+
+# main routine starts here
+yes_no_list = ["yes", "no"]
+payment_list = ["cash", "credit"]
