9f7f2dd0488b13af2778c72a2b52f08252f22cef
reillymoonen <135081213+reillymoonen@users.noreply.github.com>
2024-06-12 09:17:02 +1200

    lkjtrgntreignrel

--- 11_export_v1.py
+++ 11_export_v1.py
@@ -54,3 +54,32 @@ mini_movie_string = pandas.DataFrame.to_string(mini_movie_frame)
 ticket_cost_heading = "\n----- Ticket Cost / Profit -----"
 total_ticket_sales = "Total Ticket Sales: ${}".format(total)
 total_profit = "Total Profit : ${}".format(profit)
+
+# edit text below!! It needs towork if we have unsold tickets
+sales_status = "\n*** All the tickets have been sold ***"
+
+winner_heading = "\n----- Raffle Winner -----"
+winner_text = "The winner of the raffle is {}. " \
+                 "They have won ${}. ie: Their ticket is " \
+                 "free!".format(winner_name, total_won)
+
+# list holding content to print / write to file
+to_write = [heading, mini_movie_string, ticket_cost_heading,
+            total_ticket_sales, total_profit, sales_status,
+            winner_heading, winner_text]
+
+# print output
+for item in to_write:
+    print(item)
+
+# write output to file
+# create file to hold data (add .txt extension)
+write_to = "{}.txt".format(filename)
+text_file = open(write_to, "w+")
+
+for item in to_write:
+    text_file.write(item)
+    text_file.write("\n")
+
+# close file
+text_file.close()
